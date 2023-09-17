449a4161f2e40b3a30f534a7c791b89caa0bb0e8
Daniel Nashed <60956913+danielnashed@users.noreply.github.com>
2023-09-16 20:11:15 -0400

    Update medicalForm.py

--- services/medicalForm.py
+++ services/medicalForm.py
@@ -1,3 +1,5 @@
 
 def medical_conditions():
-    return ["Diabetes", "Hypertension", "Asthma", "Heart Disease", "Cancer", "Stroke"]
+    return ["Alzheimer’s Disease", "Arthritis", "Asthma", "Atrial Fibrillation", "Autism Spectrum Disorders", "Cancer", "Chronic Kidney Disease", "Chronic Obstructive Pulmonary Disease", "Depression", "Diabetes", "Heart Failure", "Hepatitis (Chronic Viral B & C)", "HIV/AIDS", "Hyperlipidemia (High cholesterol)", "Hypertension (High blood pressure)", "Ischemic Heart Disease", "Osteoporosis", "Schizophrenia", "Stroke"]
+
+
